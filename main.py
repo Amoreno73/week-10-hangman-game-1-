f5b10460ae97eb8070475903ede7d3b26c77e90d
Angel Moreno <113930724+Amoreno73@users.noreply.github.com>
2022-10-27 17:23:27 +0000

    created and called functions that return the sum and division of numbers

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from methods_help import method_help
-# from functions import function
+# from methods_help import method_help
+from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers
 # from returnStuff import returnS
 
-method_help()
-# function()
+# method_help()
+function()
+test = welcome("name")
+print(test)
+sum = add_numbers(45,5)
+print(sum)
+divide = num_dividedFrom_add_numbers(45, sum)
+print(divide)
 # returnS()
